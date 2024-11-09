9660bd9a0dbb99cfbe0c20aac2a89a41f08474e8
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-11-09 22:28:26 +0900

    Updated process list API.

--- src/commands/ps.cr
+++ src/commands/ps.cr
@@ -16,14 +16,35 @@ module Build
         end
         protected def execute(input : ACON::Input::Interface, output : ACON::Output::Interface) : ACON::Command::Status
           app_name_or_id = input.option("app", type: String)
-          processes = api.list_dynos(app_name_or_id)
-          unless processes
+          if app_name_or_id.blank?
+            output.puts("<error>   Missing required option --app</error>")
+            return ACON::Command::Status::FAILURE
+          end
+          dynos = api.list_dynos(app_name_or_id)
+          unless dynos
             output.puts("<info>   No processes found for app #{app_name_or_id}</info>")
             return ACON::Command::Status::FAILURE
           end
-          processes.each do |process|
-            pp process
+
+          dynos.each do |dyno|
+            # output.puts("=== #{app_name_or_id} Processes")
+            output.puts("=== #{dyno._type.colorize.green.bold} (#{dyno.size.colorize.cyan.bold}): #{dyno.display.colorize.white.bold} (#{dyno.processes.size.colorize.yellow.bold})")
+            dyno.processes.each do |process|
+              # output.puts("  #{process.index}: #{process.status} (#{process.started_at}) #{process.restarts} restarts")
+              # Specify the expected format of the timestamp (ISO 8601 in this example)
+              started_at = Time.parse(process.started_at, "%Y-%m-%dT%H:%M:%S.%LZ", location: Time::Location::UTC)
+              status = process.status == "Running" ? "up".colorize.green : "down".colorize.red
+
+              # dotiw = (Time.utc - started_at).total_seconds.to_i.seconds
+              dotiw = distance_of_time_in_words(started_at)
+
+              output.puts "#{dyno._type.colorize(:white)}.#{process.index}: #{status} " +
+                "#{started_at.to_s.colorize(:dark_gray)} (~ #{dotiw.colorize.yellow} ago)"
+
+            end
+            output.puts "" # Line break
           end
+          return ACON::Command::Status::SUCCESS
         end
       end
       @[ACONA::AsCommand("ps:restart")]
@@ -40,6 +61,10 @@ module Build
         end
         protected def execute(input : ACON::Input::Interface, output : ACON::Output::Interface) : ACON::Command::Status
           app_name_or_id = input.option("app", type: String)
+          if app_name_or_id.blank?
+            output.puts("<error>   Missing required option --app</error>")
+            return ACON::Command::Status::FAILURE
+          end
           process_name = input.argument("process", type: String?)
           if process_name.nil?
             spin = "Restarting the application #{app_name_or_id}"
