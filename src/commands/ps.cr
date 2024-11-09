50c11d12ed5c437154dc0cf4e399636b7d4f7cb2
usiegl00 <50933431+usiegl00@users.noreply.github.com>
2024-11-09 17:09:03 +0900

    Working on process List.

--- src/commands/ps.cr
+++ src/commands/ps.cr
@@ -3,6 +3,29 @@ require "term-spinner"
 module Build
   module Commands
     module Process
+      @[ACONA::AsCommand("ps")]
+      class List < Base
+        protected def configure : Nil
+          self
+            .name("ps")
+            .description("List running processes for an application")
+            .option("app", "a", :required, "The ID or NAME of the application")
+            .help("List running processes for an application")
+            .usage("ps -a <app>")
+            .aliases(["ps:ls", "ps:list"])
+        end
+        protected def execute(input : ACON::Input::Interface, output : ACON::Output::Interface) : ACON::Command::Status
+          app_name_or_id = input.option("app", type: String)
+          processes = api.list_dynos(app_name_or_id)
+          unless processes
+            output.puts("<info>   No processes found for app #{app_name_or_id}</info>")
+            return ACON::Command::Status::FAILURE
+          end
+          processes.each do |process|
+            pp process
+          end
+        end
+      end
       @[ACONA::AsCommand("ps:restart")]
       class Delete < Base
         protected def configure : Nil
