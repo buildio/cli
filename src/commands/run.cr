249f473b5795cb192c7de1b9dce715b10274caf2
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2025-05-19 22:52:48 +0900

    Update to patched SSH2 since run stopped working with new crystal/ssh2.

--- src/commands/run.cr
+++ src/commands/run.cr
@@ -16,8 +16,9 @@ module Build
       protected def configure : Nil
         self
           .name("run")
-          .argument("cmd", :is_array, "The command to run")
+          .argument("cmd", :is_array, "The command to run", [] of String)
           .option("app", "a", :required, "The app to run the command on")
+          .option("debug", nil, :none, "Show verbose debugging information")
           .description("Run a command in a once-off dyno on the Build platform")
           .help("This command is used to run a once-off dyno on the Build platform. It is useful for running tasks that are not part of a web process type such as database migrations, console sessions, or one-off scripts. The run command takes a command to run as an argument. The command will be executed in a one-off dyno on the Build platform.")
           .usage("bash -a my-app")
@@ -37,7 +38,8 @@ module Build
           return ACON::Command::Status::FAILURE
         end
 
-        command = input.argument("cmd", type: Array(String) | Nil)
+        verbose = input.option("debug", type: Bool)
+        command_array = input.argument("cmd", type: Array(String)) rescue [] of String
 
         terminal = ACON::Terminal.new
         width = terminal.width
@@ -49,33 +51,71 @@ module Build
 
         region = app.region
         spinner.update(status: "Connecting to server")
+        
+        if verbose
+          output.puts "Verbose mode enabled"
+          output.puts "Connecting to ssh.#{region}.antimony.io:1849"
+          output.puts "App: #{app.name}"
+          output.puts "Terminal size: #{width}x#{height}"
+        end
 
         SSH2::Session.open("ssh.#{region}.antimony.io", 1849) do |session|
           spinner.update(status: "Logging in")
+          if verbose
+            output.puts "SSH connection established"
+            output.puts "Authenticating with app: #{app.name}"
+          end
+          
           begin
             session.login(app.name, user_token)
-          rescue SSH2::SessionError
+            if verbose
+              output.puts "SSH authentication successful"
+            end
+          rescue e : SSH2::SessionError
             spinner.error("Login failed")
+            if verbose
+              output.puts "SSH authentication error: #{e.message}"
+            end
             exit
           end
 
           spinner.update(status: "Opening channel")
+          if verbose
+            output.puts "Opening SSH channel"
+          end
+          
           session.open_session do |channel|
             # This command gets:   ERR -22: Unable to complete request for channel-setenv  
+            if verbose
+              output.puts "Requesting PTY with terminal type: xterm-256color"
+            end
+            
             channel.request_pty( "xterm-256color", width: width, height: height )
 
+            if verbose
+              output.puts "Setting environment variables"
+            end
+            
             channel.setenv("TERM", "xterm-256color") # This should be set with the PTY request below.
             # channel.setenv("PS1", "\[\033[01;34m\]\w\[\033[00m\] \[\033[01;32m\]$ \[\033[00m\]")
 
             # channel << "export TERM=xterm-256color\n"
             # export PS1="\[\033[01;34m\]\w\[\033[00m\] \[\033[01;32m\]$ \[\033[00m\]"
             # channel << "export PS1=\"\\[\\033[01;34m\\]\\w\\[\\033[00m\\] \\[\\033[01;32m\\]\\$ \\[\\033[00m\\]\"\n"
-            if command
+            if !command_array.empty?
               spinner.update(status: "Running command")
-              channel.command(["/cnb/lifecycle/launcher", *command].join(" "))
+              cmd_string = ["/cnb/lifecycle/launcher", *command_array].join(" ")
+              if verbose
+                output.puts "Executing command: #{cmd_string}"
+              end
+              channel.command(cmd_string)
             else
               spinner.update(status: "Starting shell")
-              channel.command("/cnb/lifecycle/launcher bash")
+              if verbose
+                output.puts "Starting interactive shell"
+              end
+              # channel.command("/cnb/lifecycle/launcher bash")
+              channel.shell
             end
             #session.blocking = false
             STDIN.noecho do
@@ -95,6 +135,9 @@ module Build
                 slice = buffer.to_slice
                 loop do
                   if channel.eof?
+                    if verbose
+                      output.puts "Channel closed by remote host"
+                    end
                     break
                   end
                   count = channel.read(slice)
